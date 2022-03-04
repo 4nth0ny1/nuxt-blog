84f03a80edf910676fdd87556217da016ddabceb
4nth0ny1 <acatullo4@gmail.com>
2022-03-03 21:29:42 -0600

    initial commit

---
+++ components/AppHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <header class="header">
+      <h1>meh blog</h1>
+      <ul>
+          <li><nuxt-link class="link" to="/">home</nuxt-link></li>
+          <li><nuxt-link class="link" to="/about">about</nuxt-link></li>
+          <li><nuxt-link class="link" to="/blog">blog</nuxt-link></li>
+      </ul>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "AppHeader"
+}
+</script>
+
+<style>
+    .header {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .header ul {
+        display: flex;
+        flex-direction: row;
+    }
+
+    h1 {
+        margin: 10px;
+    }
+
+    li {
+        list-style-type: none;
+        margin: 10px;
+    }
+
+    .link {
+        color: black;
+        text-decoration: none;
+    }
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <AppHeader />
+        <nuxt/>
+    </div>
+</template>
+
+<script>
+    import AppHeader from "../components/AppHeader"
+    export default {
+        components: {
+            AppHeader
+        }
+    }
+</script>
+
+<style>
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+</style>
